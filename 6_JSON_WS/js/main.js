8b98b8cf7eb4c8b92f7e0dc64345114ef4791ad0
SleepwalkerPWD <37107643+SleepwalkerPWD@users.noreply.github.com>
2018-12-21 17:45:12 +0300

    Second_Commit

--- 6_JSON_WS/js/main.js
+++ 6_JSON_WS/js/main.js
@@ -6,7 +6,7 @@ let getJSONws = () => {
     return new Promise(resolve => {
         wsock.onopen = () => {
             wsock.send("give_json");
-            wsock.onmessage = event => {
+            wsock.onmessage = (event) => {
                 wsock.close();
                 resolve(JSON.parse(event.data));
             };
@@ -18,7 +18,20 @@ let getJSONws = () => {
 //Асинхронная функция
 async function start() {
     let json = await getJSONws();
-    console.log('JSON файл: ', json)
-}
+
+    json.categories.forEach(function(item, i, array){
+         for (let key in item){
+                let div = document.createElement('div');
+                div.style.width = '100%';
+                div.style.height = '100%';
+                div.style.margin = '10px';
+                div.style.color = '#0066FF';
+                document.body.appendChild(div);
+                div.innerHTML = (key + ' : ' + item[key]);
+         }
+    });
+    console.log('JSON файл: ', json);
+};
+
 
 start();
